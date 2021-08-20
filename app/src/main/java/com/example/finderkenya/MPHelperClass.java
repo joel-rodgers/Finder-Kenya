e9f25c7995b51394dc8febccfe1a08bdaa6466fb
joel-rodgers <joel.kobara@strathmore.edu>
2021-08-21 00:27:18 +0300

    Post missing person layout complete

---
+++ app/src/main/java/com/example/finderkenya/MPHelperClass.java
@@ -0,0 +1,121 @@
+package com.example.finderkenya;
+
+public class MPHelperClass {
+
+    private String victimName,obNumber,victimLastSeen,victimDob,victimDescription,victimHome,victimContact1,victimContact2,caseStatus,postImage,time,date;
+
+    public MPHelperClass(String victimName,String obNumber,String victimLastSeen,String victimDob,String victimDescription,String victimHome,String victimContact1,String victimContact2,String caseStatus,String postImage,String time,String date){
+        this.victimName = victimName;
+        this.obNumber = obNumber;
+        this.victimLastSeen = victimLastSeen;
+        this.victimDob = victimDob;
+        this.victimDescription = victimDescription;
+        this.victimHome = victimHome;
+        this.victimContact1 = victimContact1;
+        this.victimContact2 = victimContact2;
+        this.caseStatus = caseStatus;
+        this.postImage = postImage;
+        this.time = time;
+        this.date = date;
+
+    }
+    public MPHelperClass(){
+
+    }
+
+    public String getVictimName() {
+        return victimName;
+    }
+
+    public void setVictimName(String victimName) {
+        this.victimName = victimName;
+    }
+
+    public String getObNumber() {
+        return obNumber;
+    }
+
+    public void setObNumber(String obNumber) {
+        this.obNumber = obNumber;
+    }
+
+    public String getVictimLastSeen() {
+        return victimLastSeen;
+    }
+
+    public void setVictimLastSeen(String victimLastSeen) {
+        this.victimLastSeen = victimLastSeen;
+    }
+
+    public String getVictimDob() {
+        return victimDob;
+    }
+
+    public void setVictimDob(String victimDob) {
+        this.victimDob = victimDob;
+    }
+
+    public String getVictimDescription() {
+        return victimDescription;
+    }
+
+    public void setVictimDescription(String victimDescription) {
+        this.victimDescription = victimDescription;
+    }
+
+    public String getVictimHome() {
+        return victimHome;
+    }
+
+    public void setVictimHome(String victimHome) {
+        this.victimHome = victimHome;
+    }
+
+    public String getVictimContact1() {
+        return victimContact1;
+    }
+
+    public void setVictimContact1(String victimContact1) {
+        this.victimContact1 = victimContact1;
+    }
+
+    public String getVictimContact2() {
+        return victimContact2;
+    }
+
+    public void setVictimContact2(String victimContact2) {
+        this.victimContact2 = victimContact2;
+    }
+
+    public String getCaseStatus() {
+        return caseStatus;
+    }
+
+    public void setCaseStatus(String caseStatus) {
+        this.caseStatus = caseStatus;
+    }
+
+    public String getPostImage() {
+        return postImage;
+    }
+
+    public void setPostImage(String postImage) {
+        this.postImage = postImage;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/com/example/finderkenya/MissingPersons.java
@@ -0,0 +1,213 @@
+package com.example.finderkenya;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MissingPersons extends AppCompatActivity {
+     private ImageButton imageBtn;
+     private EditText victimname,ob,lastseen,dob,description,victimhome,vc1,vc2,cstatus;
+     private Button postBtn;
+
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+
+    private StorageReference mStorageRef;
+     private DatabaseReference databaseRef;
+     private FirebaseAuth mAuth;
+     private DatabaseReference mDatabaseUsers;
+     private FirebaseUser mCurrentUser;
+
+     private static final int GALLERY_REQUEST_CODE = 2;
+     private Uri uri =null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_missing_persons);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        postBtn= findViewById(R.id.postMP);
+        victimname= findViewById(R.id.victimname);
+        ob= findViewById(R.id.ob);
+        lastseen= findViewById(R.id.lastseen);
+        description= findViewById(R.id.description);
+        victimhome= findViewById(R.id.victimhome);
+        vc1= findViewById(R.id.vc1);
+        vc2= findViewById(R.id.vc2);
+        cstatus= findViewById(R.id.cstatus);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("missing_persons");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("users").child(mCurrentUser.getUid());
+
+
+        dob= findViewById(R.id.dob);
+        dob.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(v==dob){
+                            final Calendar c = Calendar.getInstance();
+                            mYear = c.get(Calendar.YEAR);
+                            mMonth = c.get(Calendar.MONTH);
+                            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                            DatePickerDialog datePickerDialog = new DatePickerDialog(MissingPersons.this, new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                    dob.setText(dayOfMonth + "-"+(month+1+"-"+year));
+                                }
+                            },mYear,mMonth,mDay);
+
+                            datePickerDialog.show();
+                        }
+
+                    }
+        });
+
+
+
+        imageBtn = findViewById(R.id.imgBtn);
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_REQUEST_CODE);
+            }
+        });
+
+
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MissingPersons.this,"POSTING...", Toast.LENGTH_LONG).show();
+                final String PostVictimName = victimname.getText().toString().trim();
+                final String PostOb = ob.getText().toString().trim();
+                final String PostLastSeen = lastseen.getText().toString().trim();
+                final String PostDob = dob.getText().toString().trim();
+                final String PostDescription = description.getText().toString().trim();
+                final String PostVictimHome = victimhome.getText().toString().trim();
+                final String PostVictimContact1 = vc1.getText().toString().trim();
+                final String PostVictimContact2 = vc2.getText().toString().trim();
+                final String PostCaseStatus = cstatus.getText().toString().trim();
+
+                java.util.Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat currentDate = new SimpleDateFormat("dd-MM-yyyy");
+                final String saveCurrentDate = currentDate.format(calendar.getTime());
+
+                java.util.Calendar calendar1 = calendar.getInstance();
+                SimpleDateFormat currentTime= new SimpleDateFormat("HH:mm");
+                final String saveCurrentTime = currentTime.format(calendar1.getTime());
+
+                if(!TextUtils.isEmpty(PostVictimName) || !TextUtils.isEmpty(PostOb) || !TextUtils.isEmpty(PostLastSeen) || !TextUtils.isEmpty(PostDob)
+                    || !TextUtils.isEmpty(PostDescription) || !TextUtils.isEmpty(PostVictimHome) || !TextUtils.isEmpty(PostVictimContact1)
+                    || !TextUtils.isEmpty(PostVictimContact2) || !TextUtils.isEmpty(PostCaseStatus)){
+
+
+                    StorageReference filepath = mStorageRef.child("post_victim_images").child(uri.getLastPathSegment());
+                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            if(taskSnapshot.getMetadata() != null){
+                                if(taskSnapshot.getMetadata().getReference() !=null){
+                                    Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                    result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(@NonNull Uri uri) {
+                                            final String imageUrl = uri.toString();
+                                            Toast.makeText(getApplicationContext(),"Succesfully Uploaded...", Toast.LENGTH_SHORT).show();
+                                            final DatabaseReference newPost = databaseRef.push();
+
+                                            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    newPost.child("victimName").setValue(victimname);
+                                                    newPost.child("obNumber").setValue(ob);
+                                                    newPost.child("victimLastSeen").setValue(lastseen);
+                                                    newPost.child("victimDob").setValue(dob);
+                                                    newPost.child("victimDescription").setValue(description);
+                                                    newPost.child("victimHome").setValue(victimhome);
+                                                    newPost.child("victimContact1").setValue(vc1);
+                                                    newPost.child("victimContact2").setValue(vc2);
+                                                    newPost.child("caseStatus").setValue(cstatus);
+                                                    newPost.child("postImage").setValue(imageUrl);
+                                                    newPost.child("uid").setValue(mCurrentUser.getUid());
+                                                    newPost.child("time").setValue(saveCurrentTime);
+                                                    newPost.child("date").setValue(saveCurrentDate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if(task.isSuccessful()){
+                                                                Intent intent = new Intent(MissingPersons.this, MainActivity.class);
+                                                                startActivity(intent);
+                                                            }
+                                                        }
+                                                    });
+
+
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+                                                    Toast.makeText(MissingPersons.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+}
